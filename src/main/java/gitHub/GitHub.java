b82a8fb99608a663ac7324c77af5bf408cb88279
Devendra0526 <131006509+Devendra0526@users.noreply.github.com>
2024-02-20 15:29:00 +0530

    Update GitHub.java

--- src/main/java/gitHub/GitHub.java
+++ src/main/java/gitHub/GitHub.java
@@ -10,7 +10,7 @@ package gitHub;
 public class GitHub {
 	public static void main (String[] args) {
 		System.out.println("First changes");
-		System.out.println("user2 changes");
+		System.out.println("user1 changes");
 		System.out.println("user2 changes");
 		System.out.println("master changes");
 	}
